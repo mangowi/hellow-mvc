760d5b5c6c7bea6ac5a3601f22bbfd8ecf62b3cf
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-13 01:46:04 +0200

    Razor start

--- HelloMVC/Models/HelloModel.cs
+++ HelloMVC/Models/HelloModel.cs
@@ -12,10 +12,10 @@ namespace HelloMVC.Models
         {
             List<Car> cars = new List<Car>
             {
-                new Car {Id = 1, Make = "BMW", Model = "899" },
-                new Car {Id = 2, Make = "AMW", Model = "899" },
-                new Car {Id = 3, Make = "CMW", Model = "899" },
-                new Car {Id = 4, Make = "5MW", Model = "899" }
+                new Car {Id = 1, Make = "BMW", Model = "499" },
+                new Car {Id = 2, Make = "AUD", Model = "599" },
+                new Car {Id = 3, Make = "VoksWagon", Model = "699" },
+                new Car {Id = 4, Make = "Aston Martin", Model = "799" }
 
             };
             return cars;
